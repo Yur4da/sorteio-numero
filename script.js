6324933d347df04249df6eceb543681151b27496
Yuri Assis <yuriallan2@hotmail.com>
2023-10-17 23:36:18 -0300

    firts commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function generateNumber() {
+    const min = Math.ceil(document.querySelector('.input-min').value)
+    const max = Math.floor(document.querySelector('.input-max').value)
+
+    const result = Math.floor(Math.random() * (max - min) + min);
+
+    alert('O número sorteado foi: ' + result)
+
+}
+
+
+  
